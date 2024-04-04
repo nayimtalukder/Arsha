b68987ac02af77ecac28d6c590907b37b8302e78
nayimtalukder <dr.nayimtalukder@gmail.com>
2024-04-05 04:00:50 +0600

    Sixteen

---
+++ Javascript/Index.js
@@ -0,0 +1,63 @@
+
+
+const menu = ()=>{
+
+    const x = document.getElementById("menu");
+
+    if(x.style.display == "none"){
+
+        x.style.display = "block"
+    }
+    else{
+
+        x.style.display = "none"
+    }
+};
+
+
+
+const btn_1 = () =>{
+
+    const content_1 = document.getElementById("content_1");
+
+    if(content_1.style.display == "none"){
+
+        content_1.style.display = "block"
+    }
+    else{
+
+        content_1.style.display = "none"
+    }
+};
+
+const btn_2 = () =>{
+
+    const content_2 = document.getElementById("content_2");
+
+    if(content_2.style.display == "none"){
+
+        content_2.style.display = "block"
+    }
+    else{
+
+        content_2.style.display = "none"
+    }
+    
+};
+
+
+
+const btn_3 = () =>{
+
+    const content_2 = document.getElementById("content_3");
+
+    if(content_3.style.display == "none"){
+
+        content_3.style.display = "block"
+    }
+    else{
+
+        content_3.style.display = "none"
+    }
+    
+};
